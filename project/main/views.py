1d080d43c156e7b1db55fbc9766895b496552ae3
dreamtim1 <timofei.ryko@gmail.com>
2021-09-24 03:28:51 +0300

    testing webhooks v3

--- project/main/views.py
+++ project/main/views.py
@@ -16,7 +16,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo("newbioquest.pythonanywhere.com/")
+        repo = git.Repo('/home/newbioquest/bioquest')
         origin = repo.remotes.origin
 
         origin.pull()
